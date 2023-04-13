4adb04835d06305d50263d125b8cd0ce9d33c670
Wohlstand <admin@wohlnet.ru>
2023-04-13 12:37:25 +0300

    Updated libOPNMIDI. Again. Added missing files

--- OPNMIDI-Player/src/main/cpp/update.sh
+++ OPNMIDI-Player/src/main/cpp/update.sh
@@ -3,12 +3,13 @@
 rm -Rfv include
 rm -Rfv src
 rm -v CMakeLists.txt
+rm -v libOPNMIDIConfig.cmake.in
 
 cp -av /home/vitaly/_git_repos/libOPNMIDI/include .
 cp -av /home/vitaly/_git_repos/libOPNMIDI/src .
 cp -v /home/vitaly/_git_repos/libOPNMIDI/CMakeLists.txt .
+cp -v /home/vitaly/_git_repos/libOPNMIDI/libOPNMIDIConfig.cmake.in .
 
 cp -v /home/vitaly/_git_repos/libOPNMIDI/fm_banks/xg.wopn ../assets
 
 read -n 1
-

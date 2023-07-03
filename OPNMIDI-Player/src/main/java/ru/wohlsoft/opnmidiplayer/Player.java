0513e32017e62293155bf5fae0be7e74b2b826b7
Wohlstand <admin@wohlnet.ru>
2023-07-03 03:42:23 +0300

    Fixed the work on Android 11+

--- OPNMIDI-Player/src/main/java/ru/wohlsoft/opnmidiplayer/Player.java
+++ OPNMIDI-Player/src/main/java/ru/wohlsoft/opnmidiplayer/Player.java
@@ -19,6 +19,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.InputType;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -40,11 +41,6 @@ import androidx.core.app.ActivityCompat;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Locale;
@@ -56,6 +52,7 @@ public class Player extends AppCompatActivity
     public static final int READ_PERMISSION_FOR_BANK = 1;
     public static final int READ_PERMISSION_FOR_MUSIC = 2;
     public static final int READ_PERMISSION_FOR_INTENT = 3;
+    public static final int NOTIFICATIONS_PERMISSION_FOR_INTENT = 4;
 
     private PlayerService m_service = null;
     private volatile boolean m_bound = false;
@@ -612,6 +609,8 @@ public class Player extends AppCompatActivity
                 OnOpenBankFileClick(view);
             }
         });
+
+        checkNotificationsPermissions(NOTIFICATIONS_PERMISSION_FOR_INTENT);
     }
 
 
@@ -753,53 +752,104 @@ public class Player extends AppCompatActivity
         }
     }
 
+    private void checkNotificationsPermissions(int requestCode)
+    {
+        if(Build.VERSION.SDK_INT < 33)
+            return; /* Has no effect, it's a brand-new permission type on Android 13 */
+        final int grant = PackageManager.PERMISSION_GRANTED;
+        final String postNotify = Manifest.permission.POST_NOTIFICATIONS;
+        if(ContextCompat.checkSelfPermission(this, postNotify) == grant)
+            return;
+        // Should we show an explanation?
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, postNotify))
+        {
+            // Show an explanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+            AlertDialog.Builder b = new AlertDialog.Builder(this);
+            b.setTitle("Permission denied");
+            b.setMessage("Sorry, but permission is denied!\n"+
+                    "Please, check the Post Notifications access permission to the application!");
+            b.setNegativeButton(android.R.string.ok, null);
+            b.show();
+        }
+        else
+            ActivityCompat.requestPermissions(this, new String[] { postNotify }, requestCode);
+    }
+
     private boolean checkFilePermissions(int requestCode)
     {
         final int grant = PackageManager.PERMISSION_GRANTED;
+        final String exStorage = Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
-        if (Build.VERSION.SDK_INT >= 23)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            return false; /* Has no effect, the manage file storage permission is used instead of this */
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
         {
-            final String exStorage = Manifest.permission.READ_EXTERNAL_STORAGE;
-            if (ContextCompat.checkSelfPermission(this, exStorage) == grant) {
+            if(ContextCompat.checkSelfPermission(this, exStorage) == grant)
                 Log.d(LOG_TAG, "File permission is granted");
-            } else {
+            else
                 Log.d(LOG_TAG, "File permission is revoked");
-            }
         }
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-        {
-            final String exStorage = Manifest.permission.READ_EXTERNAL_STORAGE;
-            if((ContextCompat.checkSelfPermission(this, exStorage) == grant))
-                return false;
+        if(ContextCompat.checkSelfPermission(this, exStorage) == grant)
+            return false;
 
-            // Should we show an explanation?
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, exStorage))
-            {
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                AlertDialog.Builder b = new AlertDialog.Builder(this);
-                b.setTitle("Permission denied");
-                b.setMessage("Sorry, but permission is denied!\n"+
-                        "Please, check the Read Extrnal Storage permission to application!");
-                b.setNegativeButton(android.R.string.ok, null);
-                b.show();
-                return true;
-            }
-            else
-            {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this, new String[] { exStorage }, requestCode);
-                //MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
-                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
+        // Should we show an explanation?
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, exStorage))
+        {
+            // Show an explanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+            AlertDialog.Builder b = new AlertDialog.Builder(this);
+            b.setTitle("Permission denied");
+            b.setMessage("Sorry, but permission is denied!\n"+
+                    "Please, check the External Storage access permission to the application!");
+            b.setNegativeButton(android.R.string.ok, null);
+            b.show();
             return true;
         }
+        else
+        {
+            // No explanation needed, we can request the permission.
+            ActivityCompat.requestPermissions(this, new String[] { exStorage }, requestCode);
+            // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE
+            // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
 
-        return false;
+        return true;
+    }
+
+    public boolean hasManageAppFS()
+    {
+        if(Build.VERSION.SDK_INT >= 30)
+        {
+            if(Environment.isExternalStorageManager())
+                return true;
+
+            AlertDialog.Builder b = new AlertDialog.Builder(this);
+            b.setTitle(R.string.managePermExplainTitle);
+            b.setMessage(R.string.managePermExplainText);
+            b.setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int whichButton)
+                {
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                    String pName = getPackageName();
+                    Uri uri = Uri.fromParts("package", pName, null);
+                    intent.setData(uri);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            });
+
+            b.show();
+            return false;
+        }
+        return true;
     }
 
     @Override
@@ -807,25 +857,28 @@ public class Player extends AppCompatActivity
                                            @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (grantResults.length > 0 &&
-            permissions[0].equals(Manifest.permission.READ_EXTERNAL_STORAGE) &&
-            grantResults[0] == PackageManager.PERMISSION_GRANTED
-        ) {
-            if (requestCode == READ_PERMISSION_FOR_BANK) {
-                openBankDialog();
-            } else if (requestCode == READ_PERMISSION_FOR_MUSIC) {
-                openMusicFileDialog();
-            } else if (requestCode == READ_PERMISSION_FOR_INTENT) {
-                handleFileIntent();
-            }
-        }
+        if(grantResults.length == 0)
+            return;
+        if(!permissions[0].equals(Manifest.permission.READ_EXTERNAL_STORAGE))
+            return;
+        if(grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            return;
+        if(!hasManageAppFS())
+            return;
+
+        if(requestCode == READ_PERMISSION_FOR_BANK)
+            openBankDialog();
+        else if (requestCode == READ_PERMISSION_FOR_MUSIC)
+            openMusicFileDialog();
+        else if (requestCode == READ_PERMISSION_FOR_INTENT)
+            handleFileIntent();
     }
 
 
     public void OnOpenBankFileClick(View view)
     {
         // Here, thisActivity is the current activity
-        if(checkFilePermissions(READ_PERMISSION_FOR_BANK))
+        if(checkFilePermissions(READ_PERMISSION_FOR_BANK) || !hasManageAppFS())
             return;
         openBankDialog();
     }
@@ -937,7 +990,7 @@ public class Player extends AppCompatActivity
 
     public void OnOpenFileClick(View view) {
         // Here, thisActivity is the current activity
-        if(checkFilePermissions(READ_PERMISSION_FOR_MUSIC))
+        if(checkFilePermissions(READ_PERMISSION_FOR_MUSIC) || !hasManageAppFS())
             return;
         openMusicFileDialog();
     }
@@ -966,7 +1019,7 @@ public class Player extends AppCompatActivity
         String scheme = intent.getScheme();
         if(scheme != null)
         {
-            if(checkFilePermissions(READ_PERMISSION_FOR_INTENT))
+            if(checkFilePermissions(READ_PERMISSION_FOR_INTENT) || !hasManageAppFS())
                 return;
             if(scheme.equals(ContentResolver.SCHEME_FILE))
             {

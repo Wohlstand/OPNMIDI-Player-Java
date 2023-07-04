8b83763919c243b96bfc25d14cc1e34dc6999b05
Wohlstand <admin@wohlnet.ru>
2023-07-04 19:16:55 +0300

    Removed unused imports

--- OPNMIDI-Player/src/main/java/ru/wohlsoft/opnmidiplayer/PlayerService.java
+++ OPNMIDI-Player/src/main/java/ru/wohlsoft/opnmidiplayer/PlayerService.java
@@ -8,7 +8,6 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Binder;

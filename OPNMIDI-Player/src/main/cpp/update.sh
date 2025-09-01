fff832be0273ce71ba0597a35c8a09aafff3af92
Wohlstand <admin@wohlnet.ru>
2025-09-01 08:57:07 +0300

    libOPNMIDI: Added missing CMake check

--- OPNMIDI-Player/src/main/cpp/update.sh
+++ OPNMIDI-Player/src/main/cpp/update.sh
@@ -2,6 +2,7 @@
 
 rm -Rfv include
 rm -Rfv src
+rm -Rfv cmake
 rm -v CMakeLists.txt
 rm -v libOPNMIDIConfig.cmake.in
 
@@ -9,6 +10,9 @@ cp -av /home/vitaly/_git_repos/libOPNMIDI/include .
 cp -av /home/vitaly/_git_repos/libOPNMIDI/src .
 cp -v /home/vitaly/_git_repos/libOPNMIDI/CMakeLists.txt .
 cp -v /home/vitaly/_git_repos/libOPNMIDI/libOPNMIDIConfig.cmake.in .
+
+mkdir -p ./cmake
+cp -av /home/vitaly/_git_repos/libOPNMIDI/cmake/checks ./checks
 
 cp -v /home/vitaly/_git_repos/libOPNMIDI/fm_banks/xg.wopn ../assets
 

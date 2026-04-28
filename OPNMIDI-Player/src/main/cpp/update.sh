d47c7e0ecbca83bac021f8685d148ca58d4b0e2b
Wohlstand <admin@wohlnet.ru>
2026-04-28 16:39:22 +0300

    Updated libOPNMIDI

--- OPNMIDI-Player/src/main/cpp/update.sh
+++ OPNMIDI-Player/src/main/cpp/update.sh
@@ -6,15 +6,15 @@ rm -Rfv cmake
 rm -v CMakeLists.txt
 rm -v libOPNMIDIConfig.cmake.in
 
-cp -av /home/vitaly/_git_repos/libOPNMIDI/include .
-cp -av /home/vitaly/_git_repos/libOPNMIDI/src .
-cp -v /home/vitaly/_git_repos/libOPNMIDI/CMakeLists.txt .
-cp -v /home/vitaly/_git_repos/libOPNMIDI/libOPNMIDIConfig.cmake.in .
+cp -av ../../../../../libOPNMIDI/include .
+cp -av ../../../../../libOPNMIDI/src .
+cp -v ../../../../../libOPNMIDI/CMakeLists.txt .
+cp -v ../../../../../libOPNMIDI/libOPNMIDIConfig.cmake.in .
 
 mkdir -p ./cmake
-cp -av /home/vitaly/_git_repos/libOPNMIDI/cmake/checks ./cmake/
+cp -av ../../../../../libOPNMIDI/cmake/checks ./cmake/
 
-cp -v /home/vitaly/_git_repos/libOPNMIDI/fm_banks/xg.wopn ../assets
+cp -v ../../../../../libOPNMIDI/fm_banks/xg.wopn ../assets
 
 echo "Press any key..."
 read -n 1
